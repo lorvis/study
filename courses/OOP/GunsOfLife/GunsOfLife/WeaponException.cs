80344810a532be00b32f4dd9a8b84300aeee8fa8
lorvis <remenocor@gmail.com>
2016-10-29 01:14:31 +0300

    added OOP Lab2

---
+++ courses/OOP/GunsOfLife/GunsOfLife/WeaponException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunsOfLife
+{
+
+    class WeaponException : Exception
+    {
+        Weapon weapon = null;
+        public WeaponException()
+        {
+
+        }
+
+        public WeaponException(string message) : base(message)
+        {
+
+        }
+
+        public WeaponException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+
+        public WeaponException(string message, Exception inner, Weapon weapon) : base(message, inner)
+        {
+            this.weapon = weapon;
+        }
+    }
+}
